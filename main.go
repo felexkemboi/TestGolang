1b70d7deb2beb6516a3c9bca4b9d234cd3bf94b9
felexkemboi <felokemboi10@gmail.com>
2020-08-21 11:42:06 +0300

    function to get a post

--- main.go
+++ main.go
@@ -23,6 +23,51 @@ func main(){
 
 	router :=mux NewRouter()
 
+	func createPost(w http.ResponseWriter, r *http.Request){
+		stmt, err := db.Prepare("INSERT INTO posts(title) VALUES(?)")
+
+		if err != nil{
+			panic(err.Error())
+		}
+
+		body,err := ioutil.ReadAll(r.Body)
+		if err := nil(
+			panic(err.Error())
+		)
+
+		keyVal := make(map[string]string)
+		json.Unmarshal(body,&keyVal )
+
+		_,err = stmt.Exec(title)
+		if err != nil{
+			panic(err.Error())
+		}
+
+		fmt.Fprint(w, "New post Created!")
+	}
+
+	func getPost(w http.ResponseWriter, r *http.Resquest){
+		w.Header().Set("Content-Type", "application/json")
+		params := mux.Vars(r)
+
+		result,err := db.Query("SELECT id,title FROM posts WHERE id = ?", params["id"])
+	    if err != nil{
+			panic(err.Error())
+		}
+
+		defer result.Close()
+
+		var post post
+
+		for result.Next(){
+			err != nil{
+				panic(err.Error())
+			}
+		}
+
+		json.NewEncoder(w).NewEncoder(w).Encode(post)
+	}
+
 	router.HandleFunc("/posts", getPosts).Methods("GET")
   	router.HandleFunc("/posts", createPost).Methods("POST")
   	router.HandleFunc("/posts/{id}", getPost).Methods("GET")
